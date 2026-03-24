7a64b64a1774fd0880f5658178ef3f8f3d5e30ef
Oliver Stalder <ostalder@outlook.com>
2026-03-24 15:04:12 +0100

    show all maxima on dfdv plot

--- LangmuirExcitation.jl
+++ LangmuirExcitation.jl
@@ -3,6 +3,7 @@ using DataInterpolations
 using Peaks
 using GLMakie #, MakieExtra
 #using Makie
+using PCHIPInterpolation
 
 include("load_guisdap.jl")
 include("utils.jl")
@@ -53,31 +54,69 @@ v = v_middle[abs.(E) .< 200]
 E    = E_ev(v, c.me_ev)
 grad[abs.(E) .< 3, :, :] .= NaN
 
-if showplots
-    ih = 395
-    it = 36
-    lines(E, grad[:, ih, it])
+dfdvmax = Matrix{Any}(undef, size(grad, 2), size(grad, 3))
+i_dfdvmax = Matrix{Any}(undef, size(grad, 2), size(grad, 3))
+v_dfdvmax = Matrix{Any}(undef, size(grad, 2), size(grad, 3))
+for it in axes(psd.t_run, 1)
+    for ih in axes(psd.h_atm, 1)
+        #ih = 395
+        #it = 36
+        imax, hmax = findmaxima(grad[:, ih, it]) |> peakheights(; min = 0)
+
+        if showplots    
+          lines(E, grad[:, ih, it])
+        end
+        if showplots
+            fig, ax, hm = heatmap(E, psd.h_atm/1e3, grad[:, :, it], colorrange = (-1e20, 1e20),)
+            xlims!(-200, 200)
+            Colorbar(fig[1, 2], hm)
+            display(fig)
+        end
+        if showplots
+            fig, ax, lin = lines(E, grad[:, ih, it])
+            scatter!(E[imax], hmax)
+            display(fig)
+        end
+
+        dfdvmax[ih, it] = hmax
+        i_dfdvmax[ih, it] = imax
+        v_dfdvmax[ih, it] = v[imax]
+
+        #im = 3
+        #vmax = v[imax[im]]
+        #dfdvmax = hmax[im]
+        #E_ev(vmax, c.me_ev)
+    end
 end
 
+ii  = i_dfdvmax[:, end]
+it = 36
+showplots = true
 if showplots
     fig, ax, hm = heatmap(E, psd.h_atm/1e3, grad[:, :, it], colorrange = (-1e20, 1e20),)
     xlims!(-200, 200)
     Colorbar(fig[1, 2], hm)
+    E_profiles = E_ev.(v_dfdvmax[:, it], c.me_ev)
+    nmax = maximum(length.(E_profiles[:]))
+    E_mat = stack(rpad_array.(E_profiles, nmax, NaN))
+    [scatter!(Ep, psd.h_atm/1e3, marker = 'x') for Ep in eachrow(E_mat)]
     display(fig)
 end
 
-imax, hmax = findmaxima(grad[:, ih, it])
-lines(E, grad[:, ih, it])
-scatter!(E[imax], hmax)
-
-
-#for (imax, hmax) in findmaxima(grad[:, ih, it])
-#   if hmax <= 0 continue end
-
-im = 3
-vmax = v[imax[im]]
-dfdvmax = hmax[1]
-E_ev(vmax, c.me_ev)
+if showplots
+    fig, ax, hm = heatmap(Ekin, psd.h_atm/1e3, psd.F[:, :, it], 
+       colorrange = (1e20, 1e28),
+        #colorrange = (1e24, 1e26), 
+        colorscale = log10,
+    )
+    xlims!(-200, 200)
+    Colorbar(fig[1, 2], hm)
+    E_profiles = E_ev.(v_dfdvmax[:, it], c.me_ev)
+    nmax = maximum(length.(E_profiles[:]))
+    E_mat = stack(rpad_array.(E_profiles, nmax, NaN))
+    [scatter!(Ep, psd.h_atm/1e3, marker = 'x') for Ep in eachrow(E_mat)]
+    display(fig)
+end
 
 
 #load guisdap data
@@ -107,6 +146,10 @@ t_start, t_end, h_median, ne_median, Te_median, h_mad, ne_mad, Te_mad =
     average_guisdap(joinpath.(guisdap_dir, load_files))
 gd = load_guisdap.(joinpath.(guisdap_dir, load_files))
 
+#interpolate guisdap data to psd height resolution
+ne_itp = PCHIPInterpolation(ne_median, h_median)(psd.h_atm)
+Te_itp = PCHIPInterpolation(Te_median, h_median)(psd.h_atm)
+
 if showplots
     ne_mad_lower = copy(ne_mad)
     ne_mad_lower[(ne_median - ne_mad) .< 0] .= ne_median[(ne_median - ne_mad) .< 0] ./10
@@ -121,29 +164,55 @@ if showplots
         scatter!(m.ne, m.h_param/1e3, color = "black", alpha = 0.3
         )
     end
+    lines!(ne_itp, psd.h_atm/1e3)
     axislegend(ax)
     display(fig)
 end
 
-ih = 25
 
-wp = plasma_freq(ne_median[ih], c.me)
-vth = thermal_velocity.(Te, c.me)
+if showplots
+    Te_mad_lower = copy(Te_mad)
+    Te_mad_lower[(Te_median - Te_mad) .< 0] .= Te_median[(Te_median - Te_mad) .< 0] ./10
+    fig, ax, lin = scatterlines(Te_median, h_median/1e3, 
+        marker = 'x', 
+        label = "median",
+        #axis = (xscale = log10, #limits = ((1e6, 2e12), nothing)
+    #),
+    )
+    errorbars!(Te_median, h_median/1e3, Te_mad_lower, Te_mad, direction = :x; color = :red) # same low and high error
+    for m in gd
+        scatter!(m.Te, m.h_param/1e3, color = "black", alpha = 0.3
+        )
+    end
+    lines!(Te_itp, psd.h_atm/1e3)
+    axislegend(ax)
+    display(fig)
+end
 
-vth = vth[ih]
 
-k = -2000:2000
-lines(k, k*vmax)
-lines!(k, sqrt.(wp^2 .+ 3/2*vth.^2*k.^2))
+wp = plasma_freq.(ne_itp, c.me)
+vth = thermal_velocity.(Te_itp, c.me)
+
+
+if showplots
+    ih = 370
+    vth_ = vth[ih]
+    wp_ = wp[ih]
+    k = -2000:2000
+    fig, ax, lin = lines(k, k*vmax)
+    lines!(k, sqrt.(wp_.^2 .+ 3/2*vth_.^2 .*k.^2))
+    display(fig)
+end
 
 #solve for k and omega
 #k^2 vmax^2 .- 3/2*vth.^2*k.^2 3/2 = wp^2
 #k^2 = wp^2 / (vmax^2 .- 3/2*vth.^2)
-k_growth = wp / sqrt(vmax^2 - 3/2*vth^2)
+k_growth = wp[ih] / sqrt(vmax^2 - 3/2*vth[ih]^2)
 w_growth = k_growth * vmax
 
 #growth rate!
 #double check!!
-gamma = pi * wp^2 / k_growth^2 * dfdvmax
+gamma = pi * wp[ih]^2 / k_growth^2 * dfdvmax
 
-#check with iescat wavenumber
+#check with eiscat wavenumber
+end
