b683efa592a0bcd4dfcc3ccbbda22fc4245c8f1d
Oliver Stalder <ostalder@outlook.com>
2026-03-25 11:55:51 +0100

    cleaning up

--- LangmuirExcitation.jl
+++ LangmuirExcitation.jl
@@ -16,13 +16,14 @@ showplots = true
 guisdap_dir = "analyzed_parameters/2018-12-07_folke_6.4@42mb"
 files = filter(x -> x[end-3:end] == ".mat", readdir(guisdap_dir))
 
+# 3 different time intervals to try out:
 interval1 = 143:161
 interval2 = 292:310
 interval3 = 246:264
 
-load_files = files[interval1]
+load_files = files[interval3]
 
-if false
+if showplots
     file = files[290]
     ex_time, h_param, ne, Te = load_guisdap(joinpath(guisdap_dir, file))
     println(ex_time)
@@ -34,7 +35,6 @@ if false
     end
 end
 
-# 3 different time intervals to try out:
 t_start, t_end, h_median, ne_median, Te_median, h_mad, ne_mad, Te_mad = 
     average_guisdap(joinpath.(guisdap_dir, load_files))
 gd = load_guisdap.(joinpath.(guisdap_dir, load_files))
@@ -87,156 +87,173 @@ wp = plasma_freq.(ne_itp, c.me)
 vth = thermal_velocity.(Te_itp, c.me)
 
 
-
 psd_dir = "Afven Train PSB/psd/"
-#for psd_file in readdir(psd_dir)
+psd_files = filter(x-> contains(x, ".mat"), readdir(psd_dir))
 # loop over all files
+for psd_f in psd_files
+    println("Processing ", psd_f)
+    psd_file = joinpath(psd_dir, psd_f)
+    #psd_file = "Afven Train PSB/psd/psd-30.mat"
+    # Load the .mat file
+    psd_data = matread(psd_file)
 
-# Load the .mat file
-psd_file = "Afven Train PSB/psd/psd-30.mat"
-psd_data = matread(psd_file)
-
-# Convert psd_data into a named tuple
-psd = NamedTuple{Tuple(Symbol(k) for k in keys(psd_data))}(values(psd_data)
-    )
-println(keys(psd))
-
-if showplots
-    fig, ax, hm = heatmap(psd.vpar_centres, psd.h_atm/1e3, psd.F[:, :, 50], 
-        colorscale = log10,
-        colorrange = (1e20, 1e28),
-        axis = (xlabel = "Velocity [m/s]",
-            ylabel = "Height [km]",
-            #xscale = Makie.Symlog10(0.01),
-            ),
+    # Convert psd_data into a named tuple
+    psd = NamedTuple{Tuple(Symbol(k) for k in keys(psd_data))}(values(psd_data)
         )
-    #xlims!(-100, 100)
-    Colorbar(fig[1, 2], hm)
-    display(fig)
-end
+    #println(keys(psd))
 
-dfpardvpar = stack(
-    [gradient_simple(psd.vpar_centres, psd.F[:, ih, it])[2]
-        for ih in eachindex(psd.h_atm),
-            it in eachindex(psd.t_run)
-        ]
-    )
-v_middle = psd.vpar_centres[1:end-1] .+ diff(psd.vpar_centres)
+    if showplots
+        fig, ax, hm = heatmap(psd.vpar_centres, psd.h_atm/1e3, psd.F[:, :, 50], 
+            colorscale = log10,
+            colorrange = (1e20, 1e28),
+            axis = (xlabel = "Velocity [m/s]",
+                ylabel = "Height [km]",
+                #xscale = Makie.Symlog10(0.01),
+                ),
+            )
+        #xlims!(-100, 100)
+        Colorbar(fig[1, 2], hm)
+        display(fig)
+    end
 
-Ekin = E_ev(psd.vpar_centres, c.me_ev)
-E    = E_ev(v_middle, c.me_ev)
+    dfpardvpar = stack(
+        [gradient_simple(psd.vpar_centres, psd.F[:, ih, it])[2]
+            for ih in eachindex(psd.h_atm),
+                it in eachindex(psd.t_run)
+            ]
+        )
+    v_middle = psd.vpar_centres[1:end-1] .+ diff(psd.vpar_centres)
 
-grad = copy(dfpardvpar[abs.(E) .< 200, :, :])
-v = v_middle[abs.(E) .< 200]
-E    = E_ev(v, c.me_ev)
-grad[abs.(E) .< 3, :, :] .= NaN
+    Ekin = E_ev(psd.vpar_centres, c.me_ev)
+    E    = E_ev(v_middle, c.me_ev)
 
-dfdvmax = Matrix{Any}(undef, size(grad, 2), size(grad, 3))
-i_dfdvmax = Matrix{Any}(undef, size(grad, 2), size(grad, 3))
-v_dfdvmax = Matrix{Any}(undef, size(grad, 2), size(grad, 3))
-showplots = false
-for it in axes(psd.t_run, 1)
-    for ih in axes(psd.h_atm, 1)
-        #ih = 395
-        #it = 36
-        imax, hmax = findmaxima(grad[:, ih, it]) |> peakheights(; min = 0)
+    grad = copy(dfpardvpar[abs.(E) .< 200, :, :])
+    v = v_middle[abs.(E) .< 200]
+    E    = E_ev(v, c.me_ev)
+    grad[abs.(E) .< 3, :, :] .= NaN
 
-        if showplots    
-          lines(E, grad[:, ih, it])
+    dfdvmax = Matrix{Any}(undef, size(grad, 2), size(grad, 3))
+    i_dfdvmax = Matrix{Any}(undef, size(grad, 2), size(grad, 3))
+    v_dfdvmax = Matrix{Any}(undef, size(grad, 2), size(grad, 3))
+    showplots = false
+    for it in axes(psd.t_run, 1)
+        for ih in axes(psd.h_atm, 1)
+            #ih = 395
+            #it = 36
+            imax, hmax = findmaxima(grad[:, ih, it]) |> peakheights(; min = 0)
+
+            if showplots    
+                lines(E, grad[:, ih, it])
+            end
+            if showplots
+                fig, ax, hm = heatmap(E, psd.h_atm/1e3, grad[:, :, it], colorrange = (-1e20, 1e20),)
+                xlims!(-200, 200)
+                Colorbar(fig[1, 2], hm)
+                display(fig)
+            end
+            if showplots
+                fig, ax, lin = lines(E, grad[:, ih, it])
+                scatter!(E[imax], hmax)
+                display(fig)
+            end
+
+            dfdvmax[ih, it] = hmax
+            i_dfdvmax[ih, it] = imax
+            v_dfdvmax[ih, it] = v[imax]
+
+            #im = 3
+            #vmax = v[imax[im]]
+            #dfdvmax = hmax[im]
+            #E_ev(vmax, c.me_ev)
         end
+    end
+
+    if false
+        it = 36
+        showplots = true
         if showplots
             fig, ax, hm = heatmap(E, psd.h_atm/1e3, grad[:, :, it], colorrange = (-1e20, 1e20),)
             xlims!(-200, 200)
-            Colorbar(fig[1, 2], hm)
+            Colorbar(fig[1, 2], hm, label = "df∥/d|v|∥")
+            E_profiles = E_ev.(v_dfdvmax[:, it], c.me_ev)
+            nmax = maximum(length.(E_profiles[:]))
+            E_mat = stack(rpad_array.(E_profiles, nmax, NaN))
+            [scatter!(Ep, psd.h_atm/1e3, marker = 'x') for Ep in eachrow(E_mat)]
             display(fig)
         end
+
         if showplots
-            fig, ax, lin = lines(E, grad[:, ih, it])
-            scatter!(E[imax], hmax)
+            fig, ax, hm = heatmap(Ekin, psd.h_atm/1e3, psd.F[:, :, it], 
+               colorrange = (1e20, 1e28),
+                #colorrange = (1e24, 1e26), 
+                colorscale = log10,
+            )
+            xlims!(-200, 200)
+            Colorbar(fig[1, 2], hm, label = "f∥")
+            E_profiles = E_ev.(v_dfdvmax[:, it], c.me_ev)
+            nmax = maximum(length.(E_profiles[:]))
+            E_mat = stack(rpad_array.(E_profiles, nmax, NaN))
+            [scatter!(Ep, psd.h_atm/1e3, marker = 'x') for Ep in eachrow(E_mat)]
             display(fig)
         end
 
-        dfdvmax[ih, it] = hmax
-        i_dfdvmax[ih, it] = imax
-        v_dfdvmax[ih, it] = v[imax]
+        if showplots
+            ih = 370
+            vmax = only(v_dfdvmax[ih, it])
+            vth_ = vth[ih]
+            wp_ = wp[ih]
+            k = -2000:2000
+            fig, ax, lin = lines(k, k*vmax)
+            lines!(k, sqrt.(wp_.^2 .+ 3/2*vth_.^2 .*k.^2))
+            display(fig)
+        end
 
-        #im = 3
-        #vmax = v[imax[im]]
-        #dfdvmax = hmax[im]
-        #E_ev(vmax, c.me_ev)
-    end
-end
+        #solve for k and omega
+        #k^2 vmax^2 .- 3/2*vth.^2*k.^2 3/2 = wp^2
+        #k^2 = wp^2 / (vmax^2 .- 3/2*vth.^2)
+        k_growth = wp[ih] / sqrt(vmax^2 - 3/2*vth[ih]^2)
+        w_growth = k_growth * vmax
 
-if false
-    it = 36
-    showplots = true
-    #if showplots
-        fig, ax, hm = heatmap(E, psd.h_atm/1e3, grad[:, :, it], colorrange = (-1e20, 1e20),)
-        xlims!(-200, 200)
-        Colorbar(fig[1, 2], hm, label = "df∥/d|v|∥")
-        E_profiles = E_ev.(v_dfdvmax[:, it], c.me_ev)
-        nmax = maximum(length.(E_profiles[:]))
-        E_mat = stack(rpad_array.(E_profiles, nmax, NaN))
-        [scatter!(Ep, psd.h_atm/1e3, marker = 'x') for Ep in eachrow(E_mat)]
-        display(fig)
-    end
+        #growth rate!
+        #double check!!
+        gamma = pi * wp[ih]^2 / k_growth^2 * dfdvmax[ih, it]
 
-    if showplots
-        fig, ax, hm = heatmap(Ekin, psd.h_atm/1e3, psd.F[:, :, it], 
-           colorrange = (1e20, 1e28),
-            #colorrange = (1e24, 1e26), 
-            colorscale = log10,
-        )
-        xlims!(-200, 200)
-        Colorbar(fig[1, 2], hm, label = "f∥")
-        E_profiles = E_ev.(v_dfdvmax[:, it], c.me_ev)
-        nmax = maximum(length.(E_profiles[:]))
-        E_mat = stack(rpad_array.(E_profiles, nmax, NaN))
-        [scatter!(Ep, psd.h_atm/1e3, marker = 'x') for Ep in eachrow(E_mat)]
-        display(fig)
+        #check with eiscat wavenumber
     end
 
-    if showplots
-        ih = 370
-        vmax = only(v_dfdvmax[ih, it])
-        vth_ = vth[ih]
-        wp_ = wp[ih]
-        k = -2000:2000
-        fig, ax, lin = lines(k, k*vmax)
-        lines!(k, sqrt.(wp_.^2 .+ 3/2*vth_.^2 .*k.^2))
-        display(fig)
+    k_growth = Vector{Any}(undef, size(psd.t_run, 1))
+    v_max = Vector{Any}(undef, size(psd.t_run, 1))
+    gamma = Vector{Any}(undef, size(psd.t_run, 1))
+
+    for it in axes(psd.t_run, 1)
+        v_profile = v_dfdvmax[:, it]
+        v_max[it] = v_profile
+        dfdvm_profile = dfdvmax[:, it]
+        #E_profiles = E_ev.(v_profile, c.me_ev)
+        nmax = maximum(length.(v_profile[:]))
+        v_mat = stack(rpad_array.(v_profile, nmax, NaN))'
+        dfdvm_mat = stack(rpad_array.(dfdvm_profile, nmax, NaN))'
+        #E_mat = stack(rpad_array.(E_profiles, nmax, NaN))
+        k_g = wp ./ sqrt.(v_mat.^2 .- 3/2*vth .^2)
+        k_growth[it] = k_g
+        w_g = k_g .* vmax
+        gamma[it] = pi * wp .^2 ./ k_g .^2 .* dfdvm_mat
     end
 
-    #solve for k and omega
-    #k^2 vmax^2 .- 3/2*vth.^2*k.^2 3/2 = wp^2
-    #k^2 = wp^2 / (vmax^2 .- 3/2*vth.^2)
-    k_growth = wp[ih] / sqrt(vmax^2 - 3/2*vth[ih]^2)
-    w_growth = k_growth * vmax
-
-    #growth rate!
-    #double check!!
-    gamma = pi * wp[ih]^2 / k_growth^2 * dfdvmax[ih, it]
-
-    #check with eiscat wavenumber
-end
-
-k_growth = Vector{Any}(undef, size(psd.t_run, 1))
-v_max = Vector{Any}(undef, size(psd.t_run, 1))
-
-for it in axes(psd.t_run, 1)
-    v_profiles = v_dfdvmax[:, it]
-    v_max[it] = v_profiles
-    #E_profiles = E_ev.(v_profiles, c.me_ev)
-    nmax = maximum(length.(v_profiles[:]))
-    v_mat = stack(rpad_array.(v_profiles, nmax, NaN))'
-    #E_mat = stack(rpad_array.(E_profiles, nmax, NaN))
-    k_growth[it] = wp ./ sqrt.(v_mat.^2 .- 3/2*vth .^2)
-    #w_growth = k_growth .* vmax
+    #save output for each file
+    psd_data["k_growth"] = k_growth
+    psd_data["v_phase"] = v_max
+    psd_data["growth_rate"] = gamma
+    
+    if !isdir(joinpath(psd_dir, "LE"))
+        mkdir(joinpath(psd_dir, "LE"))
+    end
+    matwrite(joinpath(psd_dir, "LE", psd_f[1:end-4]*"_LE.mat"), psd_data)
 end
 
 """
 it = 1:60 per file:     done
 ih= 1:406               done
 
-ifiles
+ifiles                  done
 """
